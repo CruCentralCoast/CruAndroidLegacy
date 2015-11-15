9a8838a6b0098fb04d89fba03628312b1ba4526d
Connor Batch <cbatch@calpoly.edu>
2015-11-14 16:06:29 -0800

    whoooo first commit

--- app/src/main/java/org/androidcru/crucentralcoast/MainActivity.java
+++ app/src/main/java/org/androidcru/crucentralcoast/MainActivity.java
@@ -2,9 +2,8 @@ package org.androidcru.crucentralcoast;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
@@ -53,7 +53,8 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START))
         {
             drawer.closeDrawer(GravityCompat.START);
-        } else
+        }
+        else
         {
             super.onBackPressed();
         }
@@ -91,22 +92,39 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara)
+        if (id == R.id.nav_home)
         {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery)
+        }
+        else if (id == R.id.nav_events)
         {
 
-        } else if (id == R.id.nav_slideshow)
+        }
+        else if (id == R.id.nav_cruber)
         {
 
-        } else if (id == R.id.nav_manage)
+        }
+        else if (id == R.id.nav_summer_missions)
         {
 
-        } else if (id == R.id.nav_share)
+        }
+        else if (id == R.id.nav_community_groups)
         {
 
-        } else if (id == R.id.nav_send)
+        }
+        else if (id == R.id.nav_ministry_teams)
+        {
+
+        }
+        else if (id == R.id.nav_articles)
+        {
+
+        }
+        else if (id == R.id.nav_videos)
+        {
+
+        }
+        else if (id == R.id.nav_tools)
         {
 
         }
