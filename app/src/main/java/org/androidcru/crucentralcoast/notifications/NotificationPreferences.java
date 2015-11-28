0d214c942c1c442080689a3789f4593951ce533d
Ian Meeder <imeeder@cox.net>
2015-11-28 15:34:45 -0800

    Added in push notifications. They can be sent remotely from my website at http://ianmeeder.com/pushNotification.html

---
+++ app/src/main/java/org/androidcru/crucentralcoast/notifications/NotificationPreferences.java
@@ -0,0 +1,9 @@
+package org.androidcru.crucentralcoast.notifications;
+
+public class NotificationPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
+
