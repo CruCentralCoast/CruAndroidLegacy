bcb02953dfa5410bcf51346c3b26150dc9c3742d
Connor Batch <connor.batch@gmail.com>
2015-12-02 23:59:41 -0800

    Added back all ministries

--- app/src/main/java/org/androidcru/crucentralcoast/presentation/presenters/SubscriptionsPresenter.java
+++ app/src/main/java/org/androidcru/crucentralcoast/presentation/presenters/SubscriptionsPresenter.java
@@ -15,15 +15,15 @@ public class SubscriptionsPresenter extends MvpBasePresenter<SubscriptionsView>
     {
         ArrayList<MinistrySubscription> subscriptions = new ArrayList<MinistrySubscription>();
         subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/slo-cru.png", "slo-cru"));
-        //subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/cuesta-cru.png", "cuesta-cru"));
+        subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/cuesta-cru.png", "cru-1"));
         subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/epic.png", "epic"));
         subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/destino.png", "destino"));
         subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/greek-row.png", "greek-row"));
         subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/athletes.png", "fellowship-of-christian-athletes-in-action"));
         subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/faculty-commons.png", "faculty-commons"));
-        //subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/branded.png", "branded"));
-        //subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/cuesta-north.png", "cuesta-north"));
-        //subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/hancock.png", "hancock"));
+        subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/branded.png", "branded"));
+        subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/cuesta-north.png", "cru-2"));
+        subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/hancock.png", "cru"));
         subscriptions.add(new MinistrySubscription("http://crucentralcoast.com/assets/img/landing/cru-high.png", "cru-high"));
 
         for (MinistrySubscription s : subscriptions)
