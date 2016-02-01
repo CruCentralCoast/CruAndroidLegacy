e380febe51ca5f769ff689b164d9717b8487814e
danielt105 <danielt105@gmail.com>
2016-01-31 16:55:10 -0800

    Split apart the DriverFragment, also Mitch is not helpful

---
+++ app/src/main/java/org/androidcru/crucentralcoast/presentation/views/fragments/DriverFragmentRideLoc.java
@@ -0,0 +1,169 @@
+package org.androidcru.crucentralcoast.presentation.views.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.mobsandgeeks.saripaar.Validator;
+import com.orhanobut.logger.Logger;
+
+import org.androidcru.crucentralcoast.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by main on 1/31/2016.
+ */
+public class DriverFragmentRideLoc extends Fragment implements OnMapReadyCallback {
+
+    public static final double MILE_METER_CONV = 1609.34;
+
+    @Bind(R.id.radius_field) EditText radiusField;
+
+    private int mapSetter;  /*used for setting markers in map appropriately*/
+    private SupportPlaceAutocompleteFragment departAutocompleteFragment;
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private Circle mapCircle;
+    private Marker departMarker;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        /*autocomplete*/
+        departAutocompleteFragment = new SupportPlaceAutocompleteFragment();
+        getChildFragmentManager().beginTransaction().replace(R.id.depart_place_autocomplete_layout, departAutocompleteFragment).commit();
+        /*map*/
+        mapFragment = new SupportMapFragment();
+        getChildFragmentManager().beginTransaction().replace(R.id.map, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+        return inflater.inflate(R.layout.driver_form, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+
+        /*autocomplete search*/
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                .build();
+        departAutocompleteFragment.setFilter(typeFilter);
+        departAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Logger.i("Depart", "Place: " + place.getName());
+                /*remove other departure markers on the map*/
+                if (departMarker != null) {
+                    departMarker.remove();
+                }
+                /*add new marker and circle to the map*/
+                departMarker = map.addMarker(new MarkerOptions()
+                        .position(place.getLatLng())
+                        .title("Departure Location"));
+                mapCircle = map.addCircle(new CircleOptions()
+                        .center(place.getLatLng())
+                        .radius(getMapRadius(radiusField.getText().toString())));
+            }
+
+            @Override
+            public void onError(Status status) {
+                Logger.i("ERROR:", "An error occurred: " + status);
+            }
+        });
+
+        /*radius watcher*/
+        radiusField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (mapCircle != null) {
+                    LatLng temp = mapCircle.getCenter();
+                    mapCircle.remove();
+                    mapCircle = map.addCircle(new CircleOptions()
+                            .center(temp)
+                            .radius(getMapRadius(s.toString())));
+                }
+            }
+        });
+    }
+
+    private double getMapRadius(String input)
+    {
+        double radius;
+        try
+        {
+            radius = Double.parseDouble(input.toString());
+            radius *= MILE_METER_CONV;
+        }
+        catch (NumberFormatException e)
+        {
+            /*default is 1 mile radius*/
+            radius = MILE_METER_CONV;
+            Logger.i(input + " was not a number");
+        }
+        Logger.i("using number " + radius);
+        return radius;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(departAutocompleteFragment != null)
+        {
+            departAutocompleteFragment.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        /*default*/
+        if (map == null)
+        {
+            map = googleMap;
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.30021, -120.66310), 14.0f));
+        }
+        else
+        {
+            /*hmmm what to do here*/
+            map.addMarker(new MarkerOptions()
+                    .position(new LatLng(40, 130))
+                    .title("Other Marker"));
+        }
+    }
+}
