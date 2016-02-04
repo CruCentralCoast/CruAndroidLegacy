d064944b682eccd1bcfdc8b74c89bf8ae960429b
Jon Amireh <jon.amireh@gmail.com>
2016-02-04 08:47:00 -0800

    Missed a merge block

--- app/src/main/java/org/androidcru/crucentralcoast/data/services/CruApiService.java
+++ app/src/main/java/org/androidcru/crucentralcoast/data/services/CruApiService.java
@@ -4,15 +4,11 @@ import org.androidcru.crucentralcoast.data.models.Campus;
 import org.androidcru.crucentralcoast.data.models.CruEvent;
 import org.androidcru.crucentralcoast.data.models.CruUser;
 import org.androidcru.crucentralcoast.data.models.MinistrySubscription;
-<<<<<<< HEAD
 import org.androidcru.crucentralcoast.data.models.MinistryTeam;
-=======
 import org.androidcru.crucentralcoast.data.models.Ride;
->>>>>>> ride-sharing
 
 import java.util.ArrayList;
 
-import retrofit.http.Body;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
