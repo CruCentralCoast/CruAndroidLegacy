db01d48baaafe9de9067eeb960b71a123af192d1
Jon Amireh <jon.amireh@gmail.com>
2016-02-14 18:44:02 -0800

    Animating camera on map

--- app/src/main/java/org/androidcru/crucentralcoast/presentation/util/MathUtil.java
+++ app/src/main/java/org/androidcru/crucentralcoast/presentation/util/MathUtil.java
@@ -3,10 +3,16 @@ package org.androidcru.crucentralcoast.presentation.util;
 public class MathUtil
 {
     private static final double MILE_METER_CONV = 1609.34;
+    private static final double RADIUS_EARTH = 6378000;
 
     public static double convertMilesToMeters(double miles)
     {
 
         return miles * MILE_METER_CONV;
+    }
+
+    public static double addMetersToLatitude(double originalLatitude, double meters)
+    {
+        return originalLatitude  + (meters / RADIUS_EARTH) * (180 / Math.PI);
     }
 }
