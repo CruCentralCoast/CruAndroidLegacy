901c10b21d54463a767173337d16eac7bfe7aa89
Connor Batch <connor.batch@gmail.com>
2016-02-27 15:55:12 -0800

    small stuff

--- app/src/main/java/org/androidcru/crucentralcoast/data/models/MinistryTeam.java
+++ app/src/main/java/org/androidcru/crucentralcoast/data/models/MinistryTeam.java
@@ -6,16 +6,18 @@ public class MinistryTeam
 {
     @SerializedName("_id") public String mId;
     @SerializedName("image") public CruImage cruImage;
+    @SerializedName("teamImage") public CruImage teamImage;
     @SerializedName("description") public String description;
     @SerializedName("name") public String name;
     @SerializedName("parentMinistry") public String mParentMinistryId;
 
     public MinistryTeam() {}
 
-    public MinistryTeam(String id, CruImage image, String name, String description, String parentMinistryId)
+    public MinistryTeam(String id, CruImage image, CruImage teamImage, String name, String description, String parentMinistryId)
     {
         this.mId = id;
         this.cruImage = image;
+        this.teamImage = teamImage;
         this.description = description;
         this.name = name;
         this.mParentMinistryId = parentMinistryId;
