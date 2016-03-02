4b2e685777f804f977a31399a8f55883cc2dfcb3
mitchlane <mitchlane44@gmail.com>
2016-03-02 14:42:59 -0800

    video plays on thumbnail click

--- app/src/main/java/org/androidcru/crucentralcoast/presentation/views/videos/VideosAdapter.java
+++ app/src/main/java/org/androidcru/crucentralcoast/presentation/views/videos/VideosAdapter.java
@@ -9,6 +9,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeIntents;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
 import com.google.api.services.youtube.model.SearchResult;
 import com.squareup.picasso.Picasso;
 
@@ -50,28 +52,35 @@ public class VideosAdapter extends RecyclerView.Adapter<VideosAdapter.CruVideoVi
         SearchResult searchResult = videos.get(position);
         holder.videoTitle.setText(searchResult.getSnippet().getTitle());
         Context context = holder.videoThumb.getContext();
+        holder.vID = searchResult.getId().getVideoId();
 
         Picasso.with(context)
                 .load(searchResult.getSnippet().getThumbnails().getDefault().getUrl())
                 .fit()
                 .into(holder.videoThumb);
+
+
+
+        holder.videoThumb.setOnClickListener((View v) -> {
+            //TODO not sure what to do if player cant resolve video
+            if(YouTubeIntents.canResolvePlayVideoIntentWithOptions(context))
+            {
+                context.startActivity(YouTubeIntents.createPlayVideoIntentWithOptions(context, holder.vID, true, true));
+            }
+        });
     }
 
-    public class CruVideoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    public class CruVideoViewHolder extends RecyclerView.ViewHolder
     {
         @Bind(R.id.videoTitle) TextView videoTitle;
         @Bind(R.id.videoThumb) ImageView videoThumb;
+        String vID;
 
         public CruVideoViewHolder(View rootView)
         {
             super(rootView);
             ButterKnife.bind(this, rootView);
-            rootView.setOnClickListener(this);
-        }
 
-        @Override
-        public void onClick(View v) {
-            // TODO play video on click here
         }
     }
 }
