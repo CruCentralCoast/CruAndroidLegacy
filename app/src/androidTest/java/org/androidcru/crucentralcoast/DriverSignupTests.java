be977c56359347497dafecb821f8fd44667dd10d
danielt105 <danielt105@gmail.com>
2016-03-11 02:36:44 -0800

    Added my test

---
+++ app/src/androidTest/java/org/androidcru/crucentralcoast/DriverSignupTests.java
@@ -0,0 +1,92 @@
+package org.androidcru.crucentralcoast;
+
+import android.app.Activity;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.androidcru.crucentralcoast.common.RxIdlingResource;
+import org.androidcru.crucentralcoast.presentation.views.MainActivity;
+import org.hamcrest.Matcher;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by main on 3/10/2016.
+ */
+public class DriverSignupTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    //call this so that Espresso will wait for network requests to complete before asserting UI stuff
+    @BeforeClass
+    public static void setup()
+    {
+        Espresso.registerIdlingResources(RxIdlingResource.get());
+    }
+
+    private void switchToEvents()
+    {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withText(R.string.nav_events)).perform(click());
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.close());
+    }
+
+    public static ViewAction clickChildViewWithId(final int id) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Click on a child view with specified id.";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View v = view.findViewById(id);
+                if (v != null) {
+                    v.performClick();
+                }
+            }
+        };
+    }
+
+    @Test
+    public void testCarCapacityInDriverSignup() {
+        String input = "5";
+
+        switchToEvents();
+
+        //click on first item
+        onView(withId(R.id.recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, clickChildViewWithId(R.id.rideSharingButton)));
+
+        //click on the Driver part of the alert dialog
+        onView(withText("DRIVER")).perform(click());
+
+        //change the car capacity field
+        onView(withId(R.id.car_capacity_field))
+                .perform(typeText(input), closeSoftKeyboard());
+
+        //check that the data was saved
+        onView(withId(R.id.car_capacity_field))
+                .check(matches(withText(input)));
+    }
+}
