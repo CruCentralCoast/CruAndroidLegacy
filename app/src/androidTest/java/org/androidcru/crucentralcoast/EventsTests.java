998049016d74b01a9f3eceabd30b407afd97b25d
Jon Amireh <jon.amireh@gmail.com>
2016-03-10 19:20:58 -0800

    Added comments, actual asserts

--- app/src/androidTest/java/org/androidcru/crucentralcoast/EventsTests.java
+++ app/src/androidTest/java/org/androidcru/crucentralcoast/EventsTests.java
@@ -12,8 +12,12 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.androidcru.crucentralcoast.common.RecyclerViewUtils.withRecyclerView;
+import static org.hamcrest.core.IsNot.not;
 
 @RunWith(AndroidJUnit4.class)
 public class EventsTests {
@@ -39,7 +43,23 @@ public class EventsTests {
     @Test
     public void doNothing() {
         switchEvents();
+
+        //click on first item
         onView(withId(R.id.recyclerview))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        //get view reference to first item and assert it's displayed
+        onView(withRecyclerView(R.id.recyclerview)
+                .atPositionOnView(0, R.id.eventDescription))
+                .check(matches(isDisplayed()));
+        
+        //click on first item again
+        onView(withId(R.id.recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        //get view reference to first item and assert it's not displayed
+        onView(withRecyclerView(R.id.recyclerview)
+                .atPositionOnView(0, R.id.eventDescription))
+                .check(matches(not(isDisplayed())));
     }
 }
