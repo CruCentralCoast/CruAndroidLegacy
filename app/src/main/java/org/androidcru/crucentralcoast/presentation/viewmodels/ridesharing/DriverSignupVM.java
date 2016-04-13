35cc7c7d3ac8abc3cc1a79817552f55990e4e436
danielt105 <danielt105@gmail.com>
2016-04-12 23:31:38 -0700

    fixed string length on forms

--- app/src/main/java/org/androidcru/crucentralcoast/presentation/viewmodels/ridesharing/DriverSignupVM.java
+++ app/src/main/java/org/androidcru/crucentralcoast/presentation/viewmodels/ridesharing/DriverSignupVM.java
@@ -22,6 +22,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.mobsandgeeks.saripaar.annotation.Min;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Pattern;
 import com.mobsandgeeks.saripaar.annotation.Select;
@@ -74,7 +75,7 @@ public class DriverSignupVM extends BaseRideVM {
     @Bind(R.id.time_field) @NotEmpty EditText rideTime;
     @Bind(R.id.date_field) @NotEmpty EditText rideDate;
 
-    @Bind(R.id.car_capacity_field) @NotEmpty EditText carCapacity;
+    @Bind(R.id.car_capacity_field) @NotEmpty @Min(value = 1) EditText carCapacity;
 
     @Bind(R.id.radius_field) @NotEmpty TextView radiusField;
     @Bind(com.google.android.gms.R.id.place_autocomplete_search_input) @NotEmpty EditText searchInput;
