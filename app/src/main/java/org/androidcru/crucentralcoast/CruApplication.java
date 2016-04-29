9104e52f561695c1b0d178e4a7a73f0a1eccbcd5
Jon Amireh <jon.amireh@gmail.com>
2016-04-28 17:01:49 -0700

    Fixed form button throttling, phone number crash, and YouTube video onClick

--- app/src/main/java/org/androidcru/crucentralcoast/CruApplication.java
+++ app/src/main/java/org/androidcru/crucentralcoast/CruApplication.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.multidex.BuildConfig;
 import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
