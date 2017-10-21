164979b8516a7cecf511e73e9a99ec98ca55ba22
Dylan Sun <supersun95@gmail.com>
2017-10-20 18:33:44 -0700

    Beginning of create account

---
+++ app/src/main/java/com/crucentralcoast/app/presentation/views/settings/CreateAccountActivity.java
@@ -0,0 +1,21 @@
+package com.crucentralcoast.app.presentation.views.settings;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.crucentralcoast.app.R;
+
+/**
+ * Created by Dylan on 10/16/17.
+ */
+
+public class CreateAccountActivity extends AppCompatActivity {
+   @Override
+   protected void onCreate(@Nullable Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_create_account);
+
+
+   }
+}
